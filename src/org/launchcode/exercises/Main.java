b025cdcb3100fb0d7375f4161c2bed0de266e7ee
unknown <rads.veerapaneni@gmail.com>
2023-07-22 10:39:28 -0500

    1

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,7 +1,6 @@
 package org.launchcode.exercises;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
@@ -15,17 +14,31 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
+        System.out.println(CheckFileExtension(studentFiles.get("Brad")));
+        System.out.println(Divide(6 , 0));
         // Test out your CheckFileExtension() function!
     }
 
-    public static void Divide(int x, int y)
-    {
+    public static int Divide(int x, int y) throws Error {
+        if (y == 0) {
+            throw new Error("you cannot divide by zero!");
+        }
+        return x / y;
         // Write code here!
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
-    }
+    public static int CheckFileExtension(String fileName) throws Error {
+        int score = 0;
+        if (fileName.contains(".java")) {
+             score++;
+        } else if (!fileName.contains(".java")) {
+             score--;
 
+        } else if (fileName.equals("")) {
+            score--;
+            throw new Error("Cannot be empty");
+            // System.out.println("Wrong score");
+        }
+    return score;
+    }
 }
